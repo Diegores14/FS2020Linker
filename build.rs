abf12fbbfdd5c3e31dd8717f7fb107103c6fc8ff
Juan Tamayo <juan.yepes@instaleap.io>
2021-11-13 14:21:55 -0500

    Logic to convert cpp file in rust code [1/2] (#1)

---
+++ build.rs
@@ -0,0 +1,17 @@
+extern crate bindgen;
+use std::{env, path::PathBuf};
+
+fn main() {
+    println!("cargo:rustc-link-search=sim_connect_cpp/lib");
+    println!("cargo:rustc-link-lib=static=SimConnect");
+
+    let bindings = bindgen::Builder::default()
+        .header("sim_connect_cpp/include/SimConnect.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
